29ede33a75550dbc8fc8950d5d0a29ff15789890
Muhammad Talha Abbas <70336405+mtalhaabbas@users.noreply.github.com>
2021-08-30 12:39:40 +0500

    Updates

---
+++ precache-manifest.945d3de4c8c9f5f1e36e5629f84b8452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c8f0b7a27982d1bed3dbfc30bb09d5",
+    "url": "/covid-19-cases-tracker/index.html"
+  },
+  {
+    "revision": "de0fa1b421ff321b020e",
+    "url": "/covid-19-cases-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d02fc7f5bd6df34e59a6",
+    "url": "/covid-19-cases-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "de0fa1b421ff321b020e",
+    "url": "/covid-19-cases-tracker/static/js/2.f72ff126.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-cases-tracker/static/js/2.f72ff126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d02fc7f5bd6df34e59a6",
+    "url": "/covid-19-cases-tracker/static/js/main.c012a614.chunk.js"
+  },
+  {
+    "revision": "ac2772e50fb3ee1c21e3",
+    "url": "/covid-19-cases-tracker/static/js/runtime-main.edec5f8a.js"
+  }
+]);
